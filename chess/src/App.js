a80a946a08924a0eeedf4b5521d33e39c6cf033c
TorinSamples <torin.samples@gmail.com>
2024-02-11 19:11:02 -0800

    starting code

--- chess/src/App.js
+++ chess/src/App.js
@@ -1,2 +1,10 @@
-import { useState } from 'react';
+import { Container } from "@mui/material";
+import Game from './Game';
 
+export default function App() {
+    return (
+        <Container>
+            <Game />
+        </Container>
+    );
+}
